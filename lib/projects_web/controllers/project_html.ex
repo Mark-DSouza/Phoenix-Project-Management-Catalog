5173ec211bc237320806679ef6affc49efe237a4
Mark-DSouza <markdsouza434@gmail.com>
2023-11-16 21:15:46 +0530

    Change priority from radio buttons to select dropdown

--- lib/projects_web/controllers/project_html.ex
+++ lib/projects_web/controllers/project_html.ex
@@ -10,4 +10,18 @@ defmodule ProjectsWeb.ProjectHTML do
   attr :action, :string, required: true
 
   def project_form(assigns)
+
+  def priority_opts(changeset) do
+    existing_priority = changeset |> Ecto.Changeset.get_change(:priority, [])
+
+    Enum.map(
+      Ecto.Enum.values(Projects.Checklist.Project, :priority),
+      fn priority ->
+      [
+        key: priority |> to_string() |> String.capitalize(),
+        value: priority,
+        selected: priority == existing_priority
+      ] end
+    )
+  end
 end
